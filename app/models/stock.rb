f27e59c99166a0f76995c2d7cf9ae8781eb6e8eb
Edmundo Pichardo <ce.pichardo@gmail.com>
2016-10-12 02:12:40 -0400

    Revisa el Modelo Stock, las llamadas multuples al api tienen problema, es mejor ahcer una sola llamada

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,35 +1,41 @@
+# NOTA IMPORTANTE ESTE API ES INESTABLE
+# Hay que hacer varios REFRESH ojo con eso
+# Hay que monitorear todas las variables.
 class Stock < ActiveRecord::Base
 
-  # Vamos a definir un metodo que suaremso depues
-
-  # Busca directamente en la DB
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
   end
 
-  # Busca directamente en el API
   def self.new_from_lookup(ticker_symbol)
     looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-    return looked_up_stock
-    # En caso de que el nombre sea nil
+    # return looked_up_stock
     return nil unless looked_up_stock.name
-
+    # symbol = looked_up_stock.symbol
+    # name = looked_up_stock.name
+    # looked_up_stock
     new_stock = new(ticker: looked_up_stock.symbol, name: looked_up_stock.name)
-    new_stock.last_price = new_stock.price
-    # Vamos a definir el metodo price de aqui arriba
+  #   Ahora vamos a crear el price method de esta misma clase, que no tenemos
+    new_stock.last_price = new_stock.price(looked_up_stock)
     new_stock
+  #   looked_up_stock.open
   end
 
-  def price
-    closing_price = StockQuote::Stock.quote(ticker).close
-  #   Esto puede devolver nil
-    return "#{closing_price} (Closing)" if closing_price
+#   Definamos el price method
+  def price(stock)
+    # return ticker
+    # closing_price = StockQuote::Stock.quote(ticker).close
+    closing_price = stock.close
+    return "#{closing_price} (Closing)" if closing_price # Si no es nil
 
-    opening_price = StockQuote::Stock.quote(ticker).open
-    return "#{opening_price} (Opening)" if opening_price
+    # opening_price  = StockQuote::Stock.quote(ticker).open
+    opening_price  = stock.open
+    return "#{opening_price} (Opening)" if opening_price #Si no es nil
 
-    'Unavailable'
-
+  #   En otraspalabras es no disponible
+    "No disponible"
   end
+
+#   Ahora vamos a def otro metodo llamado find_by_ticker(symbol)
 
 end
